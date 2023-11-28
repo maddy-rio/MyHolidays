e235a826595438ebb0a16a0a7febf7c6c10974f3
Maddy Rio <madison-rio@hotmail.com>
2023-11-28 20:44:42 +1300

    Initial Commit

---
+++ MyHolidays/MyHolidaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHolidaysApp.swift
+//  MyHolidays
+//
+//  Created by Maddy Rio on 28/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MyHolidaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyHolidaysUITests/MyHolidaysUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MyHolidaysUITestsLaunchTests.swift
+//  MyHolidaysUITests
+//
+//  Created by Maddy Rio on 28/11/23.
+//
+
+import XCTest
+
+final class MyHolidaysUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
